b1c1bceb6c970e4d11c84d5787fce5b64d2fab90
A Happy User <auser@nothing.none>
2016-03-18 01:13:53 -0700

    Initial commit

---
+++ lab3/ex.d
@@ -0,0 +1,13 @@
+
+ex.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	4c 8b 0c 25 20 23 60 	mov    0x602320,%r9
+   7:	00 
+   8:	4c 89 0c 25 08 23 60 	mov    %r9,0x602308
+   f:	00 
+  10:	68 20 10 40 00       	pushq  $0x401020
+  15:	c3                   	retq   
---
+++ lab3/ex.s
@@ -0,0 +1,3 @@
+mov $0x0c0b72cc58146fa2, %rax
+push $0x400ef3
+retq
